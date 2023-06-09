3af9fd8b1ab558a93905d4e288db65fde3c8fc0f
Ishana Jain <ishanajain99@gmail.com>
2023-06-09 15:44:41 -0700

    urls commit

--- stock/urls.py
+++ stock/urls.py
@@ -7,9 +7,12 @@ from django.conf.urls.static import static
 app_name = 'stock'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
     path('stocks/', views.stock_list, name='stock_list'),
     path('stocks/import/', views.stock_detail, name='stock_detail'),
+    path('accounts/register/', views.register, name='register'),
+    path('transaction/list/', views.transaction_list, name = 'transaction_list'),
+    path('transaction/create/', views.create_transaction, name = "create_transaction"),
     
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
